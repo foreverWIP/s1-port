83ffeca98b85d49be789fb488123c46e2866e352
Amy Wright <amywrightmail@protonmail.com>
2025-01-02 00:06:00 -0800

    fix typo in plc ramloc

--- sonic1c/ramlocs.h
+++ sonic1c/ramlocs.h
@@ -4,4 +4,5 @@
 
 #define v_256x256 RAMLOC32(0x0000)
 #define v_16x16 RAMLOC32(0xb000)
-#define v_plc_buffer RAMLOC32(0xf6e0)
+#define v_plc_buffer RAMLOC32(0xf680)
+#define v_random RAMLOC32(0xf636)
